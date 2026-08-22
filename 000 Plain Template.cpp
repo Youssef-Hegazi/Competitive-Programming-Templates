dacc056da7dd2421f520d4dcd76d41bcee37de69
Youssef-Hegazi <yhegazi0@gmail.com>
2026-08-22 13:34:43 +0300

    Create 000 Plain Template.cpp

---
+++ 000 Plain Template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+const ll MOD=998244353,INF=1e18;
+void fastIO() {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+
+void preCompute() {
+    
+}
+
+void solve() {
+    
+}
+
+int main() {
+    fastIO();
+    preCompute();
+    ll TestCases=1;
+    cin>>TestCases;
+    while(TestCases--) {
+        solve();
+    }
+}
